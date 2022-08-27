8b32a8f5cd82764bab757ee7156a3d828e1d2150
Inocente <andry.inocente@alunos.sc.senac.br>
2022-08-26 21:03:57 -0300

    Nova Classe Carro e nova impressão

---
+++ src/github_aula1/Carro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github_aula1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author andry.inocente
+ */
+public class Carro {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System.in);
+    }
+    
+}
